a41b8dde5053d51aaa9a73a7e7c3ca9029de13d6
hanachin <hanachin@gmail.com>
2011-04-24 18:46:55 +0900

    add lat? difinition.

--- func.scm
+++ func.scm
@@ -2,3 +2,17 @@
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
+
+;; p.16 lat?
+;; (define lat?
+;;   (lambda (l)
+;;     (cond ((null? l) #t)
+;;           ((atom? (car l)) (lat? (cdr l)))
+;;           (else #f))))
+
+;; my lat?
+(define lat?
+  (lambda (l)
+    (cond ((null? l) #t)
+          ((not (atom? (car l))) #f)
+          (else (lat? (cdr l))))))
