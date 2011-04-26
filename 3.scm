0fcd35322d1ba6d3e031e92100e58bbcfc9860d3
hanachin <hanachin@gmail.com>
2011-04-26 17:46:31 +0900

    add test program for chapter 3

---
+++ 3.scm
@@ -0,0 +1,12 @@
+(use gauche.test)
+(add-load-path ".")
+(load "func")
+
+(test-start "chapter 3")
+(test-section "rember")
+(test* "(rember 'mint '(lamb chops and mint jelly))" '(lamb chops and jelly) (rember 'mint '(lamb chops and mint jelly)))
+(test* "(rember 'mint '(lamb chops and mint flavored mint jelly))" '(lamb chops and flavored mint jelly) (rember 'mint '(lamb chops and mint flavored mint jelly)))
+(test* "(rember 'toast '(bacon lettuce and tomato))" '(bacon lettuce and tomato) (rember 'toast '(bacon lettuce and tomato)))
+(test* "(rember 'cup '(coffee cup tea cup and hick cup))" '(coffee tea cup and hick cup) (rember 'cup '(coffee cup tea cup and hick cup)))
+
+(test-end)
