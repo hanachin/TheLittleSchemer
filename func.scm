4eebc321e0041e0031f6a9672c49082bafbc6242
hanachin <hanachin@gmail.com>
2011-04-29 23:15:50 +0900

    p46, p48 firsts

--- func.scm
+++ func.scm
@@ -30,3 +30,9 @@
           ((eq? (car lat) a) (cdr lat))
           (else (cons (car lat)
                       (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond ((null? l) '())
+          (else (cons (car (car l))
+                      (firsts (cdr l)))))))
