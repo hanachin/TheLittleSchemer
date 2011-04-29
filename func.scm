15bb1524f475d3169f9a839ad751896a232d18ae
hanachin <hanachin@gmail.com>
2011-04-29 22:31:35 +0900

    p38 rember (p35 bug fixed)

--- func.scm
+++ func.scm
@@ -28,4 +28,5 @@
   (lambda (a lat)
     (cond ((null? lat) '())
           ((eq? (car lat) a) (cdr lat))
-          (else (rember a (cdr lat))))))
+          (else (cons (car lat)
+                      (rember a (cdr lat)))))))
