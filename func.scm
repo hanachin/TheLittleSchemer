1dbeb3619763dc53c94969c66934c479db3d377d
hanachin <hanachin@gmail.com>
2011-04-30 23:32:43 +0900

    p53 subst2

--- func.scm
+++ func.scm
@@ -68,3 +68,13 @@
           ((eq? (car lat) old) (cons new (cdr lat)))
           (else (cons (car lat)
                       (subst new old (cdr lat)))))))
+
+;; p.53 (simplify)
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond ((null? lat) '())
+          ((or (eq? (car lat) o1)
+               (eq? (car lat) o2))
+           (cons new (cdr lat)))
+          (else (cons (car lat)
+                      (subst new o1 o2 (cdr lat)))))))
