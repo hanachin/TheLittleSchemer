9530f16ad6b449e449728d153ab181f75588f883
hanachin <hanachin@gmail.com>
2011-05-01 00:04:39 +0900

    p58 define multiinsertL

--- func.scm
+++ func.scm
@@ -94,3 +94,11 @@
           ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
           (else (cons (car lat)
                       (multiinsertR new old (cdr lat)))))))
+
+;; p.58
+(define multiinsertL
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+          (else (cons (car lat)
+                      (multiinsertL new old (cdr lat)))))))
