77f366f7bd4f3000a8928d603d0910598a324b48
hanachin <hanachin@gmail.com>
2011-04-30 12:27:11 +0900

    p52 insertL (simplify ver)

--- func.scm
+++ func.scm
@@ -52,3 +52,11 @@
           ((eq? (car lat) old) (cons old (cons new (cdr lat))))
           (else (cons (car lat)
                       (insertR new old (cdr lat)))))))
+
+;; p.52 insertL (simplify)
+(define insertL
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new lat))
+          (else (cons (car lat)
+                      (insertL new old (cdr lat)))))))
