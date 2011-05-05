2d78c98a6e2c37046fd2574bd2572d21b5bfea97
hanachin <hanachin@gmail.com>
2011-05-06 01:45:04 +0900

    use test-util for chapter 2 test

--- 2.scm
+++ 2.scm
@@ -1,26 +1,25 @@
 (use gauche.test)
 (add-load-path ".")
 (load "func")
+(load "test-util")
 
 (test-start "chapter 2")
 
-(test-section "lat?")
-(test* "(lat? '(Jack Sprat could eat no chicken fat))" #t (lat? '(Jack Sprat could eat no chicken fat)))
-(test* "(lat? '((Jack) Sprat could eat no chicken fat))" #f (lat? '((Jack) Sprat could eat no chicken fat)))
-(test* "(lat? '(Jack (Sprat could) eat no chicken fat))" #f (lat? '((Jack) Sprat could eat no chicken fat)))
-(test* "(lat? '())" #t (lat? '()))
-(test* "(lat? '(bacon and eggs))" #t (lat? '(bacon and eggs)))
-(test* "(lat? '(bacon (and eggs)))" #f (lat? '(bacon (and eggs))))
+(f-test
+ (lat? ['(Jack Sprat could eat no chicken fat) => #t]
+       ['((Jack) Sprat could eat no chicken fat) => #f]
+       ['(Jack (Sprat could) eat no chicken fat) => #f]
+       ['() => #t]
+       ['(bacon and eggs) => #t]
+       ['(bacon (and eggs)) => #f])
+ 
+ (or [(null? '()) (null? '(d e f g)) => #t]
+     [(null? '(a b c)) (null? '()) => #t]
+     [(null? '(a b c)) (null? '(atom)) => #f])
 
-(test-section "or")
-(test* "(or (null? '()) (null? '(d e f g)))" #t (or (null? '()) (null? '(d e f g))))
-(test* "(or (null? '(a b c)) (null? '()))" #t (or (null? '(a b c)) (null? '())))
-(test* "(or (null? '(a b c)) (null? '(atom)))" #f (or (null? '(a b c)) (null? '(atom))))
-
-(test-section "member?")
-(test* "(member? 'tea '(coffee tea or milk))" #t (member? 'tea '(coffee tea or milk)))
-(test* "(member? 'poached '(fried eggs and scrambled eggs))" #f (member? 'poached '(fried eggs and scrambled eggs)))
-(test* "(member? 'meat '(mashed potatoes and meat gravy))" #t (member? 'meat '(mashed potatoes and meat gravy)))
-(test* "(member? 'liver '(bagels and lox))" #f (member? 'liver '(bagels and lox)))
+ (member? ['tea '(coffee tea or milk) => #t]
+          ['poached '(fried eggs and scrambled eggs) => #f]
+          ['meat '(mashed potatoes and meat gravy) => #t]
+          ['liver '(bagels and lox) => #f]))
 
 (test-end)
