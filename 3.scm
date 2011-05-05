625310389beae607dfe219791c3f2ded347295c7
hanachin <hanachin@gmail.com>
2011-05-06 01:34:57 +0900

    use test-util for chapter 3 test

--- 3.scm
+++ 3.scm
@@ -1,71 +1,65 @@
 (use gauche.test)
 (add-load-path ".")
 (load "func")
+(load "test-util")
 
 (test-start "chapter 3")
-(test-section "rember")
-(test* "(rember 'mint '(lamb chops and mint jelly))" '(lamb chops and jelly) (rember 'mint '(lamb chops and mint jelly)))
-(test* "(rember 'mint '(lamb chops and mint flavored mint jelly))" '(lamb chops and flavored mint jelly) (rember 'mint '(lamb chops and mint flavored mint jelly)))
-(test* "(rember 'toast '(bacon lettuce and tomato))" '(bacon lettuce and tomato) (rember 'toast '(bacon lettuce and tomato)))
-(test* "(rember 'cup '(coffee cup tea cup and hick cup))" '(coffee tea cup and hick cup) (rember 'cup '(coffee cup tea cup and hick cup)))
-(test* "(rember 'bacon '(bacon lettuce and tomato))" '(lettuce and tomato) (rember 'bacon '(bacon lettuce and tomato)))
-(test* "(rember 'and '(bacon lettuce and tomato))" '(bacon lettuce tomato) (rember 'and '(bacon lettuce and tomato)))
-(test* "(rember 'sauce '(soy sauce and tomato sauce))" '(soy and tomato sauce) (rember 'sauce '(soy sauce and tomato sauce)))
-
-(test-section "firsts")
-(test* "(firsts '((apple peach pumpkin) (plum pear cherry) (grape raisin pea) (bean carrot eggplant)))"
-       '(apple plum grape bean)
-       (firsts '((apple peach pumpkin)
-                 (plum pear cherry)
-                 (grape raisin pea)
-                 (bean carrot eggplant))))
-(test* "(firsts '((a b) (c d) (e f)))" '(a c e) (firsts '((a b) (c d) (e f))))
-(test* "(firsts '())" '() (firsts '()))
-(test* "(firsts '((five plums) (four) (eleven green oranges)))" '(five four eleven) (firsts '((five plums) (four) (eleven green oranges))))
-(test* "(firsts '(((five plums) four) (eleven green oranges) ((no) more)))"
-       '((five plums) eleven (no))
-       (firsts '(((five plums) four)
-                 (eleven green oranges)
-                 ((no) more))))
-
-(test-section "seconds")
-(test* "(seconds '((a b) (c d) (e f)))" '(b d f) (seconds '((a b) (c d) (e f))))
-
-(test-section "insertR")
-(test* "(insertR 'topping 'fudge '(ice cream with fudge for dessert))" '(ice cream with fudge topping for dessert) (insertR 'topping 'fudge '(ice cream with fudge for dessert)))
-;; Gauche
-;; (test* "(insertR '|jalapeño| 'and '(tacos tamales and salsa))" '(tacos tamales and |jalapeño| salsa) (insertR '|jalapeño| 'and '(tacos tamales and salsa)))
-(test* "(insertR 'jalapeno 'and '(tacos tamales and salsa))" '(tacos tamales and jalapeno salsa) (insertR 'jalapeno 'and '(tacos tamales and salsa)))
-(test* "(insertR 'e 'd '(a b c d f g d h))" '(a b c d e f g d h) (insertR 'e 'd '(a b c d f g d h)))
-
-;; my insertL test
-(test-section "insertL")
-(test* "(insertL 'topping 'fudge '(ice cream with fudge for dessert))" '(ice cream with topping fudge for dessert) (insertL 'topping 'fudge '(ice cream with fudge for dessert)))
-(test* "(insertL 'jalapeno 'and '(tacos tamales and salsa))" '(tacos tamales jalapeno and salsa) (insertL 'jalapeno 'and '(tacos tamales and salsa)))
-(test* "(insertL 'e 'd '(a b c d f g d h))" '(a b c e d f g d h) (insertL 'e 'd '(a b c d f g d h)))
-
-(test-section "subst")
-(test* "(subst 'topping 'fudge '(ice cream with fudge for dessert))" '(ice cream with topping for dessert) (subst 'topping 'fudge '(ice cream with fudge for dessert)))
-
-(test-section "subst2")
-(test* "(subst2 'vanilla 'chocolate 'banana '(banana ice creamwith chocolate topping))"
-       '(vanilla ice creamwith chocolate topping)
-       (subst2 'vanilla 'chocolate 'banana '(banana ice creamwith chocolate topping)))
-
-(test-section "multirember")
-(test* "(multirember 'cup '(coffee cup tea cup and hick cup))" '(coffee tea and hick) (multirember 'cup '(coffee cup tea cup and hick cup)))
-
-;; my multiinsertR test
-(test-section "multiinsertR")
-(test* "(multiinsertR 'e 'd '(a b c d f g d h))" '(a b c d e f g d e h) (multiinsertR 'e 'd '(a b c d f g d h)))
-(test* "(multiinsertR 'y 'y '(w r y y y y y))" '(w r y y y y y y y y y y) (multiinsertR 'y 'y '(w r y y y y y)))
-
-(test-section "multiinsertL")
-(test* "(multiinsertL 'fried 'fish '(chips and fish or fish and fried))" '(chips and fried fish or fried fish and fried) (multiinsertL 'fried 'fish '(chips and fish or fish and fried)))
-(test* "(multiinsertL 'j 'o '(o o))" '(j o j o) (multiinsertL 'j 'o '(o o)))
-
-(test-section "multisubst")
-(test* "(multisubst 'o 'i '(i p p a i))" '(o p p a o) (multisubst 'o 'i '(i p p a i)))
-(test* "(multisubst 'o 'i '())" '() (multisubst 'o 'i '()))
+(f-test
+ (rember ['mint '(lamb chops and mint jelly) => '(lamb chops and jelly)]
+         ['mint '(lamb chops and mint flavored mint jelly)
+                => '(lamb chops and flavored mint jelly)]
+         ['toast '(bacon lettuce and tomato) => '(bacon lettuce and tomato)]
+         ['cup '(coffee cup tea cup and hick cup)
+               => '(coffee tea cup and hick cup)]
+         ['bacon '(bacon lettuce and tomato) => '(lettuce and tomato)]
+         ['and '(bacon lettuce and tomato) => '(bacon lettuce tomato)]
+         ['sauce '(soy sauce and tomato sauce) => '(soy and tomato sauce)])
+ 
+ (firsts ['((apple peach pumpkin) (plum pear cherry)
+            (grape raisin pea) (bean carrot eggplant))
+          => '(apple plum grape bean)]
+         ['((a b) (c d) (e f)) => '(a c e)]
+         ['() => '()]
+         ['((five plums) (four) (eleven green oranges)) => '(five four eleven)]
+         ['(((five plums) four) (eleven green oranges) ((no) more))
+          => '((five plums) eleven (no))])
+ 
+ (seconds ['((a b) (c d) (e f)) => '(b d f)])
+ 
+ (insertR ['topping 'fudge '(ice cream with fudge for dessert)
+                    => '(ice cream with fudge topping for dessert)]
+          ;; Gauche
+          ;; ['|jalapeño| 'and '(tacos tamales and salsa)
+          ;;              => (tacos tamales and |jalapeño| salsa)]
+          ['jalapeno 'and '(tacos tamales and salsa)
+                     => '(tacos tamales and jalapeno salsa)]
+          ['e 'd '(a b c d f g d h) => '(a b c d e f g d h)])
+ 
+ ;; my insertL test
+ (insertL ['topping 'fudge '(ice cream with fudge for dessert)
+                    => '(ice cream with topping fudge for dessert)]
+          ['jalapeno 'and '(tacos tamales and salsa)
+                     => '(tacos tamales jalapeno and salsa)]
+          ['e 'd '(a b c d f g d h) => '(a b c e d f g d h)])
+ 
+ (subst ['topping 'fudge '(ice cream with fudge for dessert)
+                  => '(ice cream with topping for dessert)])
+ 
+ (subst2 ['vanilla 'chocolate 'banana '(banana ice creamwith chocolate topping)
+                   => '(vanilla ice creamwith chocolate topping)])
+ 
+ (multirember ['cup '(coffee cup tea cup and hick cup)
+                    => '(coffee tea and hick)])
+ 
+ ;; my multiinsertR test
+ (multiinsertR ['e 'd '(a b c d f g d h) => '(a b c d e f g d e h)]
+               ['y 'y '(w r y y y y y) => '(w r y y y y y y y y y y)])
+ 
+ (multiinsertL ['fried 'fish '(chips and fish or fish and fried)
+                       => '(chips and fried fish or fried fish and fried)]
+               ['j 'o '(o o) => '(j o j o)])
+ 
+ (multisubst ['o 'i '(i p p a i) => '(o p p a o)]
+             ['o 'i '() => '()]))
 
 (test-end)
