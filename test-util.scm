ddd95adb7f156ed08c0c902ce18a69d1b86cec96
hanachin <hanachin@gmail.com>
2011-05-06 17:26:59 +0900

    add syntax eqt

--- test-util.scm
+++ test-util.scm
@@ -1,12 +1,14 @@
+(define-syntax eqt
+  (syntax-rules ()
+    ((_ expr expected)
+     (test* (quote expr) expected expr))))
+
 (define-syntax f-test
   (syntax-rules (=>)
     ((_ (f (=> a args ...) ...) ...)
-     (let-syntax
-         ((eqt (syntax-rules ()
-                 ((_ expr expected)
-                  (test* (quote expr) expected expr)))))
+     (begin
        (begin
          (test-section (symbol->string (quote f)))
          (eqt (f args ...) a) ...) ...))
-    ((_ (f (args rest ...) ...) ...)
-     (begin (f-test (f (rest ... args) ...)) ...))))
+  ((_ (f (args rest ...) ...) ...)
+   (begin (f-test (f (rest ... args) ...)) ...))))
