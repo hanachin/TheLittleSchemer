72cf0bb617fcca7a3e5ec399e04a0105117456f6
hanachin <hanachin@gmail.com>
2011-05-06 17:31:54 +0900

    add number? test case to chapter 4 test

--- 4.scm
+++ 4.scm
@@ -8,4 +8,11 @@
 (test-section "atom?")
 (eqt (atom? 14) #t)
 
+(test-section "number?")
+(eqt (number? -3) #t)
+(eqt (number? 3.1415) #t)
+(eqt (and (number? -3)
+          (number? 3.1415))
+     #t)
+
 (test-end)
