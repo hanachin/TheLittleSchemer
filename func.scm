8b08251674037abd315a0d776c26c51ac6aa426b
hanachin <hanachin@gmail.com>
2011-05-08 00:09:47 +0900

    p73 tup+(simplify)

--- func.scm
+++ func.scm
@@ -161,8 +161,7 @@
 ;; p.73
 (define tup+
   (lambda (tup1 tup2)
-    (cond ((and (null? tup1) (null? tup2)) '())
-          ((null? tup1) tup2)
+    (cond ((null? tup1) tup2)
           ((null? tup2) tup1)
           (else (cons (o+ (car tup1) (car tup2))
                       (tup+ (cdr tup1) (cdr tup2)))))))
