d53b325840f6c5bf837b52e8885b070308cf3979
hanachin <hanachin@gmail.com>
2011-05-08 00:09:14 +0900

    p73 tup+

--- func.scm
+++ func.scm
@@ -158,9 +158,11 @@
     (cond ((zero? m) 0)
           (else (o+ n (o* n (sub1 m)))))))
 
-;; p.70
+;; p.73
 (define tup+
   (lambda (tup1 tup2)
     (cond ((and (null? tup1) (null? tup2)) '())
+          ((null? tup1) tup2)
+          ((null? tup2) tup1)
           (else (cons (o+ (car tup1) (car tup2))
                       (tup+ (cdr tup1) (cdr tup2)))))))
