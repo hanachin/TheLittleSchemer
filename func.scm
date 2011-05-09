56b642154eac83707f6b50a07cee127dce9f262f
hanachin <hanachin@gmail.com>
2011-05-10 07:05:32 +0900

    p88 insertL*

--- func.scm
+++ func.scm
@@ -312,3 +312,14 @@
                              (subst* new old (cdr l))))))
           (else (cons (subst* new old (car l))
                       (subst* new old (cdr l)))))))
+
+;; p.88
+(define insertL*
+  (lambda (new old l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+           (cond ((eq? (car l) old)
+                  (cons new (cons old (insertL* new old (cdr l)))))
+                 (else (cons (car l) (insertL* new old (cdr l))))))
+          (else (cons (insertL* new old (car l))
+                      (insertL* new old (cdr l)))))))
