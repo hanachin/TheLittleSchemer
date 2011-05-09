b564bef7420fd2fa1088209c212ae6d3561ec0e8
hanachin <hanachin@gmail.com>
2011-05-10 07:09:52 +0900

    p89 member*

--- func.scm
+++ func.scm
@@ -323,3 +323,11 @@
                  (else (cons (car l) (insertL* new old (cdr l))))))
           (else (cons (insertL* new old (car l))
                       (insertL* new old (cdr l)))))))
+;; p.89
+(define member*
+  (lambda (a l)
+    (cond ((null? l) #f)
+          ((atom? (car l)) (or (eq? (car l) a)
+                               (member* a (cdr l))))
+          (else (or (member* a (car l))
+                    (member* a (cdr l)))))))
