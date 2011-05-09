fa24150a8cf9563116ca09b41f621763c97e93a7
hanachin <hanachin@gmail.com>
2011-05-10 06:42:38 +0900

    p83 insertR*

--- func.scm
+++ func.scm
@@ -277,3 +277,15 @@
                              (rember* a (cdr l))))))
           (else (cons (rember* a (car l))
                       (rember* a (cdr l)))))))
+
+;; p.84
+(define insertR*
+  (lambda (new old l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+           (cond ((eq? (car l) old)
+                  (cons old (cons new (insertR* new old (cdr l)))))
+                 (else (cons (car l)
+                             (insertR* new old (cdr l))))))
+          (else (cons (insertR* new old (car l))
+                      (insertR* new old (cdr l)))))))
