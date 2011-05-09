41f03e12316941eace35370940b6ad60a3357e65
hanachin <hanachin@gmail.com>
2011-05-10 06:28:51 +0900

    add rember* test case

--- 5.scm
+++ 5.scm
@@ -4,4 +4,16 @@
 (load "test-util")
 
 (test-start "chapter 5")
+(test-section "rember*")
+
+(f-test
+ (rember* ['cup '((coffee) cup ((tea) cup) (and (hick)) cup)
+                => '((coffee) ((tea)) (and (hick)))]
+          ['sauce '(((tomato sauce))
+                   ((bean) sauce)
+                   (and ((flying)) sauce))
+                 => '(((tomato))
+                      ((bean))
+                      (and ((flying))))]))
+
 (test-end)
