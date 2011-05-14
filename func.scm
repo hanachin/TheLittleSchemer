af4f4747cc0e1a4df7a84544040f482ce1d9754a
hanachin <hanachin@gmail.com>
2011-05-14 22:40:57 +0900

    p96 equal? simplify

--- func.scm
+++ func.scm
@@ -338,18 +338,13 @@
     (cond ((atom? (car l)) (car l))
           (else (leftmost (car l))))))
 
-;; p.94
+;; p.96
 (define eqlist?
   (lambda (l1 l2)
     (cond ((and (null? l1) (null? l2)) #t)
           ((or (null? l1) (null? l2)) #f)
-          ((and (atom? (car l1)) (atom? (car l2)))
-           (and (eqan? (car l1) (car l2))
-                (eqlist? (cdr l1) (cdr l2))))
-          ((or (atom? (car l1)) (atom? (car l2))) #f)
-          (else (and (eqlist? (car l1) (car l2))
-                     (eqlist? (cdr l1) (cdr l2)))))))
-
+          (else (and (equal? (car l1) (car l2))
+                     (equal? (cdr l1) (cdr l2)))))))
 ;; p.95
 (define equal?
   (lambda (s1 s2)
