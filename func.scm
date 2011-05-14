848f35e82d751c023ca4b313ba4c6c89989c73a7
hanachin <hanachin@gmail.com>
2011-05-14 22:28:36 +0900

    p94 eqlist? simplify

--- func.scm
+++ func.scm
@@ -337,3 +337,15 @@
   (lambda (l)
     (cond ((atom? (car l)) (car l))
           (else (leftmost (car l))))))
+
+;; p.94
+(define eqlist?
+  (lambda (l1 l2)
+    (cond ((and (null? l1) (null? l2)) #t)
+          ((or (null? l1) (null? l2)) #f)
+          ((and (atom? (car l1)) (atom? (car l2)))
+           (and (eqan? (car l1) (car l2))
+                (eqlist? (cdr l1) (cdr l2))))
+          ((or (atom? (car l1)) (atom? (car l2))) #f)
+          (else (and (eqlist? (car l1) (car l2))
+                     (eqlist? (cdr l1) (cdr l2)))))))
