cdb101a910417cc629de4dc22b48bc1651e54827
hanachin <hanachin@gmail.com>
2011-05-14 22:38:39 +0900

    p95 equal? simplify

--- func.scm
+++ func.scm
@@ -355,6 +355,5 @@
   (lambda (s1 s2)
     (cond ((and (atom? s1) (atom? s2))
            (eqan? s1 s2))
-          ((atom? s1) #f)
-          ((atom? s2) #f)
+          ((or (atom? s1) (atom? s2)) #f)
           (else (eqlist? s1 s2)))))
