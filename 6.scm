c9df9470c0ca190f522c928718f6126595535f29
hanachin <hanachin@gmail.com>
2011-05-19 16:29:07 +0900

    add numbered? test cases

--- 6.scm
+++ 6.scm
@@ -4,5 +4,10 @@
 (load "test-util")
 
 (test-start "chapter 6")
+(test-section "numbered?")
+(f-test
+ (numbered? [1 => #t]
+            ['(3 + (4 expt 5)) => #t]
+            ['(2 * sausage) => #f]))
 
 (test-end)
