582d4a851d0d89f166a8ff5dfd8b11ac22c50fbf
hanachin <hanachin@gmail.com>
2011-05-19 17:21:09 +0900

    add value test cases

--- 6.scm
+++ 6.scm
@@ -7,6 +7,10 @@
 (f-test
  (numbered? [1 => #t]
             ['(3 + (4 expt 5)) => #t]
-            ['(2 * sausage) => #f]))
+            ['(2 * sausage) => #f])
+ (value [13 => 13]
+        ['(1 + 3) => 4]
+        ['(1 + (3 expt 4)) => 82」
+        ['cookie => (test-error)]))
 
 (test-end)
