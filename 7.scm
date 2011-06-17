10816993bce9b96b78d460b82d625c398a5fa130
hanachin <hanachin@gmail.com>
2011-06-17 16:20:12 +0900

    add subset? test cases

--- 7.scm
+++ 7.scm
@@ -12,6 +12,15 @@
        ['(apple 3 pear 4 9 apple 3 4) => #f])
  (makeset ['(apple peach pear peach plum apple lemon peach)
            ;=> '(pear plum apple lemon peach)
-           => '(apple peach pear plum lemon)]))
+           => '(apple peach pear plum lemon)])
+ (subset? ['(5 chicken wings)
+           '(5 hamburgers
+               2 pieces fried chicken and
+               light duckling wings)
+           => #t]
+          ['(4 pounds of horseradish)
+           '(four pounds chicken and
+                  5 ounces horseradish)
+           => #f]))
 
 (test-end)
