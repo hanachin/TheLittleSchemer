b1f086d52c1a29921aa4b4b129fe3c7c433ac071
hanachin <hanachin@gmail.com>
2011-06-17 16:23:22 +0900

    change subset?

--- func.scm
+++ func.scm
@@ -424,5 +424,5 @@
 (define subset?
   (lambda (set1 set2)
     (cond ((null? set1) #t)
-          ((member? (car set1) set2) (subset? (cdr set1) set2))
-          (else #f))))
+          (else (and (member? (car set1) set2)
+                     (subset? (cdr set1) set2))))))
