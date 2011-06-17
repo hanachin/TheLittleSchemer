1b2fea586030467be72ed31dc235b3dad67bbd95
hanachin <hanachin@gmail.com>
2011-06-17 15:49:27 +0900

    add makeset test case

--- 7.scm
+++ 7.scm
@@ -9,6 +9,8 @@
  (set? ['(apple peaches apple plum) => #f]
        ['(apples peaches pears plums) => #t]
        ['() => #t]
-       ['(apple 3 pear 4 9 apple 3 4) => #f]))
+       ['(apple 3 pear 4 9 apple 3 4) => #f])
+ (makeset ['(apple peach pear peach plum apple lemon peach)
+           => '(apple peach pear plum lemon)]))
 
 (test-end)
