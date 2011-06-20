63b81f2b8bf107b06d462b553012d405f045dec6
hanachin <hanachin@gmail.com>
2011-06-20 11:44:20 +0900

    p118 intersect

--- func.scm
+++ func.scm
@@ -439,3 +439,12 @@
     (cond ((null? set1) #f)
           (else (or (member? (car set1) set2)
                     (intersect? (cdr set1) set2))))))
+
+;; p.118
+(define intersect
+  (lambda (set1 set2)
+    (cond ((null? set1) '())
+          ((member? (car set1) set2)
+           (cons (car set1)
+                 (intersect (cdr set1) set2)))
+          (else (intersect (cdr set1) set2)))))
