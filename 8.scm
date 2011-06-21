e6c9c39618d794ffbe1958d51ed39e0683e0cff4
hanachin <hanachin@gmail.com>
2011-06-22 03:59:25 +0900

    add rember-f test cases

---
+++ 8.scm
@@ -0,0 +1,14 @@
+(use gauche.test)
+(add-load-path ".")
+(load "func")
+(load "test-util")
+
+(test-start "chapter 7")
+(f-test
+ (rember-f [= 5 '(6 2 5 3) => '(6 2 3)]
+           [eq? 'jelly '(jerry beans are good)
+                => '(beans are good)]
+           [equal? '(pop corn) '(lemonade (pop corn) and (cake))
+                   => '(lemonade and (cake))])
+ )
+(test-end)
