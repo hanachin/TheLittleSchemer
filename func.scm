78c5364033cc1cfcb9930bac55792093d1950058
hanachin <hanachin@gmail.com>
2011-06-22 04:04:15 +0900

    p128 rember-f

--- func.scm
+++ func.scm
@@ -524,3 +524,11 @@
 (define one-to-one?
   (lambda (fun)
     (fun? (revrel fun))))
+
+;; p.128
+(define rember-f
+  (lambda (test? a l)
+    (cond ((null? l) '())
+          ((test? (car l) a) (cdr l))
+          (else (cons (car l)
+                      (rember-f test? a (cdr l)))))))
