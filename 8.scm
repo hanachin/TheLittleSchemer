a1768e43e7365d51bc13c5f773f22ddd00af5046
hanachin <hanachin@gmail.com>
2011-06-27 03:05:47 +0900

    add rember-f test case

--- 8.scm
+++ 8.scm
@@ -5,13 +5,9 @@
 
 (test-start "chapter 7")
 (f-test
- ;; (rember-f [= 5 '(6 2 5 3) => '(6 2 3)]
- ;;           [eq? 'jelly '(jelly beans are good)
- ;;                => '(beans are good)]
- ;;           [equal? '(pop corn) '(lemonade (pop corn) and (cake))
- ;;                   => '(lemonade and (cake))])
  (eq?-salad ['salad => #t]
             ['tuna => #f])
  (rember-eq? ['tuna '(tuna salad is good)
                     => '(salad is good)]))
+(test* "((rember-f eq?) 'tuna '(shrimp salad and tuna salad))" '(shrimp salad and salad) ((rember-f eq?) 'tuna '(shrimp salad and tuna salad)))
 (test-end)
