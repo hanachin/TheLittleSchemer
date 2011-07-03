c277529a0368d674a1cd661b28d8e92f44d71614
hanachin <hanachin@gmail.com>
2011-07-04 02:16:18 +0900

    p152 keep-looking

--- func.scm
+++ func.scm
@@ -726,3 +726,9 @@
 (define looking
   (lambda (a lat)
     (keep-looking a (pick 1 lat) lat)))
+
+;; p.152
+(define keep-looking
+  (lambda (a sorn lat)
+    (cond ((number? sorn) (keep-looking a (pick sorn lat) lat))
+          (else (eq? sorn a)))))
