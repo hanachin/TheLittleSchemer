3ad19f4fbf8e95ba71521e8ac29f98f6f62d6af5
hanachin <hanachin@gmail.com>
2011-07-04 01:50:27 +0900

    add looking test cases

---
+++ 9.scm
@@ -0,0 +1,13 @@
+(use gauche.test)
+(add-load-path ".")
+(load "func")
+(load "test-util")
+
+(test-start "chapter 9")
+(f-test
+ (looking ['caviar '(6 2 4 caviar 5 7 3)
+                   => #t]
+          ['caviar '(6 2 grits caviar 5 7 3)
+                   => #f]))
+
+(test-end)
