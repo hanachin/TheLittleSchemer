273af3515b9a168e8b1be3d2f0d6545009dbf25b
hanachin <hanachin@gmail.com>
2011-07-04 18:53:43 +0900

    add weight* test cases

--- 9.scm
+++ 9.scm
@@ -12,6 +12,8 @@
           ['caviar '(6 2 4 caviar 5 7 3)
                    => #t])
  (shift ['((a b) c) => '(a (b c))]
-        ['((a b) (c d)) => '(a (b (c d)))]))
+        ['((a b) (c d)) => '(a (b (c d)))])
+ (weight* ['((a b) c) => 7]
+          ['(a (b c)) => 5]))
 
 (test-end)
