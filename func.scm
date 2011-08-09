06a5e757356f653584b6ac2ed0bf57dfd0c33c7e
hanachin <hanachin@gmail.com>
2011-08-09 22:54:10 +0900

    p189 apply

--- func.scm
+++ func.scm
@@ -930,3 +930,10 @@
 (define non-primitive?
   (lambda (l)
     (eq? (first l) 'non-primitive)))
+
+(define apply
+  (lambda (fun vals)
+    (cond ((primitive? fun)
+           (apply-primitive (second fun) vals))
+          ((non-primitive? fun)
+           (apply-closure (second fun) vals)))))
